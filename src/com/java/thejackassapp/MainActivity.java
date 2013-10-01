e3adf9b064f09e7860c6ae1404b2e6f80707b420
dooohhead <dooohhead@gmail.com>
2013-09-30 21:18:42 -0300

    Active button code for Jackass App

--- src/com/java/thejackassapp/MainActivity.java
+++ src/com/java/thejackassapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.java.thejackassapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -19,5 +23,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    public void sendMessage(View aView) {
+    	Intent anIntent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	anIntent.putExtra(EXTRA_MESSAGE,  message);
+    	startActivity(anIntent);
+    }
 }
